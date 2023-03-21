04f0a09463c3baf48b9ce8f690a0b407d72b32f1
Nat10k <110515021+Nat10k@users.noreply.github.com>
2023-03-21 07:28:01 +0700

    fix DFS algo, combine util and utils

--- src/dfs.cs
+++ src/dfs.cs
@@ -5,20 +5,19 @@ using util;
 
 namespace Dfs {
     public class Dfs {
-        // Prioritas arah : Kanan Bawah Kiri Atas
+        // Prioritas arah : Atas Bawah Kiri Kanan
         public int[,] visited;
         public int visitCount {get; set;}
         public int stepCount {get; set;}
-        public bool back {get;set;} // simpen backtrack atau ga
         public long execTime {get;set;}
+        private int maxVisit; // jumlah maksimum visit suatu node. Untuk backtracking
         public List<Tuple<int,int>> pathResult = new List<Tuple<int, int>>();
         public Dfs(ref Matrix m) {
-            Stack<Tuple<int,int>> path = new Stack<Tuple<int,int>>();
-            Stack<Tuple<int,int>> backTrackPath = new Stack<Tuple<int,int>>();
+            Stack<Tuple<int,int>> path = new Stack<Tuple<int,int>>(); // Stack penampung path sementara
             Tuple<int,int> temp;
             visitCount = 1;
+            maxVisit = 1;
             stepCount = 0;
-            back = false;
             visited = new int[m.nRow, m.nCol];
             int i = m.startRow, j = m.startCol;
             int treasureCount = 0;
@@ -28,40 +27,45 @@ namespace Dfs {
             visited[i,j]++;
             while (treasureCount < m.totalTreasure) {
                 bool isStuck = false;
-                if (canVisit(i,j+1,m)) {
-                    j += 1;
-                } else if (canVisit(i+1,j,m)) {
-                    i += 1;
-                } else if (canVisit(i,j-1,m)) {
-                    j -= 1;
-                } else if (canVisit(i-1,j,m)) {
+                bool backTrack = false;
+                // Periksa apakah ada tetangga yang belum dikunjungi sesuai prioritas arah
+                if (canVisit(i-1,j,m,1)) {
                     i -= 1;
+                } else if (canVisit(i+1,j,m,1)) {
+                    i += 1;
+                } else if (canVisit(i,j-1,m,1)) {
+                    j -= 1;
+                } else if (canVisit(i,j+1,m,1)) {
+                    j += 1;
                 } else {
-                    isStuck = true;
-                    temp = path.Pop();
-                    Tuple<int,int> prev = path.Peek();
-                    if (m.container[i,j] == 'T' || back) {
-                        back = true;
-                        backTrackPath.Push(temp);
+                    backTrack = true;
+                    // Periksa apakah ada tetangga yang jumlah dikunjunginya lebih kecil dari maxVisit
+                    if (canVisit(i-1,j,m,maxVisit)) {
+                        i -= 1;
+                    } else if (canVisit(i+1,j,m,maxVisit)) {
+                        i += 1;
+                    } else if (canVisit(i,j-1,m,maxVisit)) {
+                        j -= 1;
+                    } else if (canVisit(i,j+1,m,maxVisit)) {
+                        j += 1;
+                    } else {
+                        isStuck = true;
+                        temp = path.Pop();
+                        Tuple<int,int> prev = path.Peek();
+                        if (m.container[i,j] == 'T') {
+                            path.Push(temp);
+                            path.Push(prev);
+                            maxVisit++;
+                        }
+                        i = prev.Item1;
+                        j = prev.Item2;
                     }
-                    i = prev.Item1;
-                    j = prev.Item2;
                 }
                 if (!isStuck) {
-                    if (back) {
-                        Stack<Tuple<int,int>> revBackTrackPath = new Stack<Tuple<int, int>>(backTrackPath.Reverse());
-                        while (backTrackPath.TryPop(out temp)) {
-                            path.Push(temp);
-                        }
-                        while(revBackTrackPath.TryPop(out temp)) {
-                            path.Push(temp);
-                        }
-                        back = false;
-                    }
-                    path.Push(new Tuple<int,int>(i,j));
-                    if (m.container[i,j] == 'T') {
+                    if (m.container[i,j] == 'T' && !backTrack) {
                         treasureCount++;
                     }
+                    path.Push(new Tuple<int,int>(i,j));
                     visitCount++;
                 }
                 visited[i,j]++;
@@ -71,12 +75,13 @@ namespace Dfs {
                 m.container[temp.Item1,temp.Item2] = 'H';
                 stepCount++;
             }
+            stepCount--;
             stopwatch.Stop();
             execTime = stopwatch.ElapsedMilliseconds;
         }
         
-        public bool canVisit(int i, int j, Matrix m) {
-            if (i < m.nRow && i >=0 && j >=0 && j <m.nCol && m.container[i,j] != 'X' && visited[i,j] == 0) {
+        private bool canVisit(int i, int j, Matrix m, int maxVisitNum) {
+            if (i < m.nRow && i >=0 && j >=0 && j <m.nCol && m.container[i,j] != 'X' && visited[i,j] < maxVisitNum) {
                 return true;
             } else {
                 return false;
@@ -93,37 +98,6 @@ namespace Dfs {
                 }
                 Console.WriteLine();
             }
-        }
-        public Stack<T> createReverseStack<T>(Stack<T> s) {
-            Stack<T> reverse = new Stack<T>();
-            T temp;
-            while(s.TryPop(out temp)) {
-                reverse.Push(temp);
-            }
-            return reverse;
-        }
-    }
-
-    class testClass {
-        public static void Main() {
-            Matrix m = new Matrix("../../test/test.txt");
-            m.printMatrix();
-            Console.WriteLine();
-            Dfs d = new Dfs(ref m);
-            m.printMatrix();
-            Console.WriteLine(d.stepCount);
-            Console.WriteLine(d.visitCount);
-            Console.WriteLine(d.execTime);
-            for (int i=0; i<d.pathResult.Count; i++) {
-                Console.Write(d.pathResult[i].Item1);
-                Console.Write(",");
-                Console.Write(d.pathResult[i].Item2);
-                if (i != d.pathResult.Count-1) {
-                    Console.Write(" - ");
-                }
-            }
-            Console.WriteLine();
-            d.printVisited();
         }
     }
 }
--- src/util.cs
+++ src/util.cs
@@ -1,6 +1,39 @@
 namespace util{
     class util{
-        public class Matrix { // Isi matriks yg diperluin kurleb gini (yg DFS)
+        public static String convertRoute(List<Tuple<int,int>> path){
+            String Route = "";
+            for(int i = 0; i < path.Count -1 ; i++){
+                if(path[i].Item1 == path[i+1].Item1 && path[i].Item2 == path[i+1].Item2 - 1){
+                    Route = Route + "R";
+                }
+                else if(path[i].Item1 == path[i+1].Item1 && path[i].Item2 == path[i+1].Item2 + 1){
+                    Route = Route + "L";
+                }
+                else if(path[i].Item1 == path[i+1].Item1-1 && path[i].Item2 == path[i+1].Item2){
+                    Route = Route + "D";
+                }
+                else if(path[i].Item1 == path[i+1].Item1+1 && path[i].Item2 == path[i+1].Item2){
+                    Route = Route + "U";
+                }
+                if(i < path.Count - 2){
+                    Route = Route + " - ";
+                }
+            }
+            return Route;
+        }
+        public static int getTreasureCount (string[,] maze){
+            int count = 0;
+            for(int i = 0; i < maze.GetLength(0); i++){
+                for(int j = 0; j < maze.GetLength(1); j ++){
+                    if(maze[i,j] == "T"){
+                        count ++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+    public class Matrix { // Isi matriks yg diperluin kurleb gini (yg DFS)
         public char[,] container = new char[100,100];
         public int startRow {get;set;} // Perlu start
         public int startCol {get;set;}
@@ -43,6 +76,5 @@ namespace util{
                 Console.WriteLine();
             }
         }
-    }
     }
 }
